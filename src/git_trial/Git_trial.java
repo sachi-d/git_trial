79372eb76eef4b9f8c6628d87c682a3b304bdc4c
Sachi <sachithradangalla@gmail.com>
2015-10-16 18:06:31 +0530

    initial commit - created new project to yas

---
+++ src/git_trial/Git_trial.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git_trial;
+
+/**
+ *
+ * @author Sachi
+ */
+public class Git_trial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        yasara yas=new yasara("yasara lol",100);
+        yas.displayinfo();
+    }
+    
+}
---
+++ src/git_trial/yasara.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git_trial;
+
+/**
+ *
+ * @author Sachi
+ */
+public class yasara {
+    String name;
+    int age;
+    
+    public yasara(String n,int a){
+        name=n;
+        age=a;
+    }
+    
+    public void displayinfo(){
+        System.out.println("My name is "+name);
+        System.out.println("I am "+age+" years old hehee");
+    }
+}
